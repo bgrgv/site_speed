b89bd96afaa81bc0361bd57fff91fb3e5bba07c8
bgrgv <46672514+bgrgv@users.noreply.github.com>
2019-03-27 01:29:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// console.log("started....");
+const electron=require("electron");
+const app=require("electron").app;
+const url=require("url");
+const path=require("path");
+const BrowserWindow=require("electron").BrowserWindow;
+const timer=require("timers")
+var notify=require("electron").notify
+var Stopwatch = require("node-stopwatch").Stopwatch;
+const { dialog } = require('electron')
+
+let win;
+
+function createWindow(){
+    win=new BrowserWindow({show:false,hasShadow:true,frame:false,maxHeight:800,maxWidth:800,resizable:false,minHeight:800,minWidth:800,webPreferences:{nodeIntegration:false}
+    ,);
+    // win.loadURL(url.format({
+    //     pathname:path.join(__dirname,"index.html"),
+    //     protocol:"file:",
+    //     slashes:true,
+    // }));
+
+    // stopwatch.start();
+
+    var stopwatch = Stopwatch.create();
+    stopwatch.start();
+    win.loadURL("https://www.amazon.com");
+    win.once('ready-to-show',()=>{
+        win.show();
+        var elapsed=stopwatch.elapsed.seconds;
+        const options={
+            title:"Site Benchmark",
+            detail:"Time taken to load the site : "+elapsed,
+        }
+        const response = dialog.showMessageBox(null,options);
+    })
+   
+    stopwatch.stop();
+    win.on("close",()=>{
+        win=null;   
+    })
+}
+
+app.on('ready',createWindow)
